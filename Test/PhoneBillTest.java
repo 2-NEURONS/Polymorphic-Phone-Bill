018270f77f2b6f65d87dc266655c9992cd7214ba
Onke-JavaProgrammeratCodeX <60272627+Onke-JavaProgrammeratCodeX@users.noreply.github.com>
2020-07-08 12:30:30 +0200

    Update PhoneBillTest.java
    Added more tests

--- Test/PhoneBillTest.java
+++ Test/PhoneBillTest.java
@@ -5,13 +5,26 @@ import static org.junit.Assert.*;
 public class PhoneBillTest
 {
     @Test
-    public void shouldIncrementTotal()
+    public void shouldIncrementTotalCost()
     {
-        PhoneBill phoneBill = new PhoneBill();
 
-        phoneBill.accept(new PhoneCall(5));
-        int result = (int)phoneBill.total();
-        assertEquals(5 , result);
+        PhoneBill phoneBill = new PhoneBill(); // PhoneBill instance
+
+        phoneBill.accept(new PhoneCall(5)); // pass a phone call
+        phoneBill.accept(new SMSBundle(8 , 1.5 )); // pass an sms bundle
+        phoneBill.accept(new DataBundle(200)); // for mega bytes less than 500mb
+
+        phoneBill.accept(new PhoneCall(10)); // accept a phone call
+        phoneBill.accept(new SMSBundle(3 , 1.76 )); // accept sms bundle
+        phoneBill.accept(new DataBundle(700));  // for mega bytes > 500mb
+
+        phoneBill.accept(new PhoneCall(15)); // accept a phone call
+        phoneBill.accept(new SMSBundle(6 , 3.3 )); // accept sms bundle
+        phoneBill.accept(new DataBundle(10000000));  // for mega bytes > 1000mb
+        int result = (int)phoneBill.total(); // accept data bundles
+        assertEquals((int)68.73 , result); // let check results
+
+
     }
 
 
