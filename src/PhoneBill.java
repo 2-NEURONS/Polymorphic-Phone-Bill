51d1c0393f66e3e8366fefdd9b25f9e3beb2ada4
Onke-JavaProgrammeratCodeX <60272627+Onke-JavaProgrammeratCodeX@users.noreply.github.com>
2020-07-08 12:16:55 +0200

    Update PhoneBill.java
    changes made :
    1. deleted public BillAction billAction as I never needed it
    2. made totalCost private

--- src/PhoneBill.java
+++ src/PhoneBill.java
@@ -1,8 +1,8 @@
 public class PhoneBill
 {
 
-  public BillAction billAction; // have access to the instance of the BillAction so you can return its total cost from total
-  public double totalCost; // total cost of the current bill
+  private double totalCost; // total cost of the current bill
+  
   public void accept(BillAction billAction)
   {
     totalCost += billAction.totalCost() ; // increment and pass total cost of the current bill to total cost
@@ -12,5 +12,5 @@ public class PhoneBill
     return totalCost;
   }
 
-
 }
+
