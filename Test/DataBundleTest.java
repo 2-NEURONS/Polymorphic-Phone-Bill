927eaa2325944189e874d7fd8e5557e4cbf91c11
Onke-JavaProgrammeratCodeX <60272627+Onke-JavaProgrammeratCodeX@users.noreply.github.com>
2020-07-08 12:28:44 +0200

    Update DataBundleTest.java
    Added more tests

--- Test/DataBundleTest.java
+++ Test/DataBundleTest.java
@@ -6,11 +6,52 @@ public class DataBundleTest
 {
 
     @Test
-    public void shouldReturnValidPrice()
-    {
-        DataBundle data = new DataBundle(800);
+    public void shouldReturn0_75ForAllMegaBytesLessThan500() {
 
-        assertEquals((int)0.55 , (int)data.getDataBundlePrice());
+        // for all bundles in this range 0 - 499MB , 501 - 1000MB , 1001 -
+        DataBundle data = null;
+        for (int bytes = 0; bytes <= 499; bytes++)  // mega bytes less than 500MB
+        {
+            data = new DataBundle(bytes);
+
+            assertEquals(0.75, data.getDataBundlePrice(), 0.0);
+
+        }
+
     }
 
-}
+    @Test
+    public void shouldReturn0_55ForAllMegaBytesGreaterThan500()
+    {
+
+        // for all bundles in this range 0 - 499MB , 501 - 1000MB , 1001 -
+        DataBundle data = null;
+        for(int bytes = 501 ; bytes <= 1000; bytes ++ ) // mega bytes greater than 500MB but less than anything greater than 1000MB
+        {
+            data = new DataBundle(bytes);
+
+            assertEquals(0.55 , data.getDataBundlePrice() , 0.0);
+
+        }
+
+
+    }
+    @Test
+    public void shouldReturn0_35ForAllMegaBytesGreaterThan1000()
+    {
+
+        // for all bundles in this range 0 - 499MB , 501 - 1000MB , 1001 -
+        DataBundle data = null;
+        for(int bytes = 1001 ; bytes <= 5000; bytes ++ ) // mega bytes greater than 1000MB
+        {
+            data = new DataBundle(bytes);
+
+            assertEquals(0.35 , data.getDataBundlePrice() , 0.0);
+
+        }
+
+
+      }
+
+
+    }
